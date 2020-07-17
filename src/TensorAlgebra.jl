b13bc634455d8cd8a9eac2bf0cb257a16500809d
Eric Forgy <Eric.Forgy@cavalre.com>
2020-07-17 01:07:09 -0700

    Add Tensor

--- src/TensorAlgebra.jl
+++ src/TensorAlgebra.jl
@@ -37,10 +37,6 @@ dual(::VectorSpace{K,Covector{K,L},L}) where {K,L} = VectorSpace{K,Vector{K},L}(
 dual(ts::ProductSpace) = ProductSpace(dual.(kind(ts))...)
 # dual(ts::ProductSpace) = ProductSpace(reverse(dual.(kind(ts)))...) # Consider reversing?
 
-Base.size(t::Tensor) = size(t.array)
-
-Base.getindex(t::Tensor,ix) = getindex(t.array, ix)
-
 Covector(vs::VectorSpace{K},a) where {K} = Covector{K,label(vs)}(a)
 
 Vector(vs::VectorSpace{K},a) where {K} = Tensor{K,1,VectorSpace{K,Covector{K,label(vs)},label(vs)}}(a)
@@ -50,22 +46,24 @@ VectorSpace(L::Symbol,::Type{K}) where {K} = VectorSpace{K,Vector{K},L}()
 ProductSpace(args::VectorSpace{K}...) where {K} = 
     ProductSpace{K,length(args),(args...,),Symbol(join(args," × "))}()
 
-# Tensor(vs::ProductSpace{K<R},a) where {K} = 
+Tensor(::P,a::Array{K,R}) where {K,R,P<:ProductSpace{K,R}} = Tensor{K,R,P}(a)
 
-domain(::Tensor{K,1,D}) where {K,D} = D()
+domain(::Tensor{K,R,D}) where {K,R,D} = D()
 
-domain(::Tensor{K,R,D}) where {K,R,D} = D
+# domain(::Tensor{K,R,D}) where {K,R,D} = D
     
 function (f::Tensor{K,R})(x::Tensor{K,R}) where {K,R}
     dual(domain(f)) === domain(x) || error("Domain mismatch")
     dot(f.array, x.array)
 end
 
+Base.size(t::Tensor) = size(t.array)
+
+Base.getindex(t::Tensor,ix::Vararg{Int}) = getindex(t.array, ix...)
+
+Base.isassigned(t::Tensor, ix::Vararg{Int}) = isassigned(t.array, ix...)
+
 Base.:^(v::AbstractSpace,::typeof(*)) = dual(v)
-
-Base.show(io::IO, vs::AbstractSpace) = print(io, label(vs))
-
-Base.show(io::IO, vs::VectorSpace{K,<:Covector{K}}) where {K} = print(io, label(vs), "^*")
 
 Base.first(ts::ProductSpace) = first(kind(ts))
 
@@ -82,5 +80,11 @@ Base.in(t::Tensor{K,1}, vs::VectorSpace{K}) where {K} = dual(domain(t)) === vs
 ×(ts1::ProductSpace{K},ts2::ProductSpace{K}) where {K} = ProductSpace((kind(ts1)..., kind(ts2)...))
 
 # ⊗(t1::Tensor{K,R1},t2::Tensor{K,R2}) where {K,R1,R2} = 
+
+Base.show(io::IO, vs::AbstractSpace) = print(io, label(vs))
+
+Base.show(io::IO, vs::VectorSpace{K,<:Covector{K}}) where {K} = print(io, label(vs), "⃰")
+
+Base.show(io::IO, vs::Type{<:AbstractSpace}) = print(io, label(vs()))
 
 end # module
